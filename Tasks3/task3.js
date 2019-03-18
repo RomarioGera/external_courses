a0814faef1812290f78c8ad7e3a0c1705d2ae8e4
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-18 21:48:06 +0300

    Create task3.js

---
+++ Tasks3/task3.js
@@ -0,0 +1,12 @@
+function t3(a, b) {
+    a = "string",
+        b = {
+            name: "Roman",
+            location: "Ryazan"
+        }
+    if (b.name == "Roman") {
+        return 'true';
+    } else {
+        return 'false';
+    }
+}
