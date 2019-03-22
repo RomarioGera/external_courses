319bb37097267200d2894ffa000083191557d19e
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-22 17:58:26 +0300

    Update task7.js

--- Tasks4/task7.js
+++ Tasks4/task7.js
@@ -2,6 +2,6 @@ function f(a, b) {
   var aLenght = a.length;
   if (a.length > b) {
     var result = a.substring(0, b) + '...'
-  } else var result = str;
+  } else var result = a;
   return result;
 }
