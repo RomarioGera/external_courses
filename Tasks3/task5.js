40ee7615c0796d6769b375325a3668033a17b0a6
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-23 20:32:49 +0300

    Update task5.js

--- Tasks3/task5.js
+++ Tasks3/task5.js
@@ -1,12 +1,14 @@
 var obj = {
-  Name: 'name',
-  Age: 'age',
-  Location: 'location'
+    Name: 'name',
+    Age: 'age',
+    Location: 'location'
 };
 
-function t5(obj1) {
-  var obj2 = obj1;
-  return obj2;
+function t5(obj) {
+    var obj2 = obj;
+    for (var key in obj2) {
+        console.log(key + obj2[key]);
+    };
 }
 
 t5(obj)
