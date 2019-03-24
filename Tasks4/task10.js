5589077adaf0f8688e002e4f8be7947316bfbf42
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-24 12:29:44 +0300

    Update task10.js

--- Tasks4/task10.js
+++ Tasks4/task10.js
@@ -1,3 +1,8 @@
 function reverseStr(str) {
-    return str.split("").reverse().join("");
+    var newStr = '', i;
+    for (i = str.length - 1; i >= 0; i--) {
+        newStr += str.charAt(i);
+    }
+    return newStr;
 }
+
