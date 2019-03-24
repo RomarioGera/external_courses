c6357ddd2ade7d262241429a586e3563bfc7193f
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-24 20:15:59 +0300

    Update Calculator.js

--- Tasks5/Calculator.js
+++ Tasks5/Calculator.js
@@ -1,1 +1,19 @@
-
+var Calculator = {
+    add: function(a, b){
+        var addResult = a + b;
+        return addResult;
+    },
+    subtract: function(a, b){
+        var subtractResult = a - b;
+        return subtractResult;
+    },
+    divide: function(a, b){
+        var divideResult = a / b;
+        return divideResult;
+    },
+    multiply: function(a, b){
+        var multiplyResult = a * b;
+        return multiplyResult;
+    },
+    getResult: 
+};
