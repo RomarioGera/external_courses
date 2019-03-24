dd9bfe9924905aef634beeea8147ae4dae23dfae
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-24 12:33:55 +0300

    Update task1.js

--- Tasks4/task1.js
+++ Tasks4/task1.js
@@ -3,7 +3,7 @@ var obj1 = Object.create(obj);
 
 function newInProto(anyObj) {
     anyObj.c = 3;
-    for (var key in anyObj) {
+    for (var key in anyObj.__proto__) {
         console.log(key);
     } return;
 }
