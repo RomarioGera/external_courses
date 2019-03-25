23c142534643567891498861028df63bb6b430b6
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-25 09:31:31 +0300

    Update task6.js

--- Tasks3/task6.js
+++ Tasks3/task6.js
@@ -1,5 +1,11 @@
-function = objectDeepClone(object1){
-object1 = [];
-var cloneObject = Object.assign({}, object1);
-return cloneObject;
-}
+    function objectDeepClone(obj) {
+        var newObj = {};
+        for (var key in obj) {
+            if (typeof obj[key] === 'object')
+                newObj[key] = objectDeepClone(obj[key])
+
+            else newObj[key] = obj[key];
+        }
+
+        return newObj;
+    }
