9ada6f622b4086518cd558b3b959ca9246ae9171
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-28 17:46:44 +0300

    Update task9.js

--- Tasks4/task9.js
+++ Tasks4/task9.js
@@ -1,3 +1,6 @@
-function reverseStr(str) {
-    return str.split("").reverse().join("");
+function wordInsert(string, insert, num){
+    var strSeparation = string.split([' ']);
+    var stringPart1 = string.slice(strSeparation[0], strSeparation[num]);
+    var stringPart2 = string.slice(strSeparation[num]);
+    return stringPart1 + insert + stringPart2; 
 }
