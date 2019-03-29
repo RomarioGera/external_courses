3792691d2590993c5aeb5b6debaa57143a9a9f9b
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-03-29 09:51:40 +0300

    Update task9.js

--- Tasks4/task9.js
+++ Tasks4/task9.js
@@ -1,6 +1,5 @@
 function wordInsert(string, insert, num){
-    var strSeparation = string.split([' ']);
-    var stringPart1 = string.slice(strSeparation[0], strSeparation[num]);
-    var stringPart2 = string.slice(strSeparation[num]);
-    return stringPart1 + insert + stringPart2; 
+    var strSeparation = string.split(' ');
+     strSeparation.splice(num +1, 0, insert);
+     return strSeparation.join(' '); 
 }
