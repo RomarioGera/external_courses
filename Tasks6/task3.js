eea75d10875d01aeddea1bd013680021c88801ff
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-01 12:22:25 +0300

    Create task3.js

---
+++ Tasks6/task3.js
@@ -0,0 +1,9 @@
+function every(arr, callback, thisArg) {
+    var i, length = arr.length;
+    for (i = 0; i < length; i = i + 1) {
+        if (!callback.call(thisArg, arr[i], i, arr)) {
+            return false;
+        }
+    }
+    return true;
+};
