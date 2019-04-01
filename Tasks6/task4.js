bacd11568ef5dbe789fa10bb29721ba7c89d6faf
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-01 12:23:54 +0300

    Create task4.js

---
+++ Tasks6/task4.js
@@ -0,0 +1,9 @@
+function filter(arr, callback, thisArg) {
+  var i, length = arr.length, results = [];
+  for (i = 0; i < length; i = i + 1) {
+    if (callback.call(thisArg, arr[i], i, arr)) {
+      results.push(arr[i]);
+    }
+  }
+  return results;
+};
