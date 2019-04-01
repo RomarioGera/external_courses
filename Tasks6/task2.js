29e9bdf7aca39e4ea4ff7770c4780e5f5dd63b0c
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-01 12:17:41 +0300

    Update task2.js

--- Tasks6/task2.js
+++ Tasks6/task2.js
@@ -9,3 +9,15 @@ function some(arr, callback) {
         return false;
     }
 }
+
+//или
+
+var some = function(arr, callback, thisArg) {
+  var i, length = arr.length;
+  for (i = 0; i < length; i = i + 1) {
+    if (callback.call(thisArg, arr[i], i, arr)) {
+      return true;
+    }
+  }
+  return false;
+};
