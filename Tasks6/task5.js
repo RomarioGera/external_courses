4199b7890dd5abc2bfe06ec06983b6225da3561f
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-03 14:24:13 +0300

    Update task5.js

--- Tasks6/task5.js
+++ Tasks6/task5.js
@@ -1,7 +1,7 @@
-function map(arr, callback, thisArg) {
+function map(arr, callback) {
   var i, length = arr.length, results = [];
   for (i = 0; i < length; i = i + 1) {
-    results.push(callback.call(thisArg, arr[i], i, arr));
+    results.push(callback(arr[i], i, arr));
   }
   return results;
 };
