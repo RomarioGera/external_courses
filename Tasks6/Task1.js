07eda3ae5ac530688c11e3c7b119a84f7014eae3
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-03 14:49:29 +0300

    Update Task1.js

--- Tasks6/Task1.js
+++ Tasks6/Task1.js
@@ -2,9 +2,20 @@ function slice(array, begin, end) {
     var arrayCopy = [];
     if (end == undefined) {
         end = 0;
-    } else {
-        for (var i = begin; i < end; i++) {
-            arrayCopy.push(array[i]);
-        }
-    } return arrayCopy;
+    }
+    if (begin > end && begin < 0 && end < 0) {
+        return [];
+    }
+
+    if (begin < 0) {
+        begin = array.length + begin
+    }
+    if (end < 0) {
+        end = array.length + end
+    }
+
+    for (var i = begin; i < end; i++) {
+        arrayCopy.push(array[i]);
+    }
+    return arrayCopy;
 }
