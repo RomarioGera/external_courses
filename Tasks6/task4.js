ad1fe2f6bf266223a7ad5420184b90e8fb597c16
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-04-03 14:23:42 +0300

    Update task4.js

--- Tasks6/task4.js
+++ Tasks6/task4.js
@@ -1,9 +1,9 @@
-function filter(arr, callback, thisArg) {
-  var i, length = arr.length, results = [];
-  for (i = 0; i < length; i = i + 1) {
-    if (callback.call(thisArg, arr[i], i, arr)) {
-      results.push(arr[i]);
+function filter(arr, callback) {
+    var i, length = arr.length, results = [];
+    for (i = 0; i < length; i = i + 1) {
+      if (callback(arr[i], i, arr)) {
+        results.push(arr[i]);
+      }
     }
-  }
-  return results;
-};
+    return results;
+  };
