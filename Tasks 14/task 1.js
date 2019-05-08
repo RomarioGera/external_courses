6eb3906ce6007a411d868aa5adbd35248c9636d5
Roman Gerasimov <48129335+RomarioGera@users.noreply.github.com>
2019-05-08 22:13:16 +0300

    Create task 1.js

---
+++ Tasks 14/task 1.js
@@ -0,0 +1,46 @@
+var Calculator = (function () {
+    let state = 0;
+
+    return {
+        add: function (value) {
+            state += value;
+            return this;
+        },
+        multiply: function (value) {
+            state *= value;
+            return this;
+        },
+        subtract: function (value) {
+            state -= value;
+            return this;
+        },
+        divide: function (value) {
+            state /= value;
+            return this;
+        },
+        reset: function () {
+            state = 0;
+            return this;
+        },
+        setState: function (value) {
+            state = value;
+        },
+        fetchData: function (callback) {
+            callback.call(this);
+        },
+        getResult: function () {
+            return state;
+        }
+    }
+})();
+
+Calculator.setState(1);
+console.log(Calculator.getResult());
+
+function callback() {
+    setTimeout(() => {
+        this.setState(500);
+        console.log(this.getResult());
+    }, 2000);
+}
+Calculator.fetchData(callback);
