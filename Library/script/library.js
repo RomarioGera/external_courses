2c64da1f0fdfdd5afd0b8ffa895ef0deb73eb0ba
Roman Gerasimov <gerasimovr@gmail.com>
2019-05-18 22:00:13 +0300

    Loading books.1

--- Library/script/library.js
+++ Library/script/library.js
@@ -1,15 +1,4 @@
-// get Library data
-
-/* const xhr = new XMLHttpRequest();
- xhr.open('get', '/script/json.json', false);
- xhr.send();
- if (xhr.status != 200) {
-     alert(xhr.status + ': ' + xhr.statusText);
- } else {
-     alert(xhr.statusText);
-  }  
-let data = JSON.parse(xhr); */
-
+//Create Books Array
 var books = [{
     "id": 1,
     "title": "Jewels of Nizam",
@@ -142,81 +131,23 @@ var books = [{
     "image_url": "http://rsu-library-api.herokuapp.com/static/images/10.jpg"
 }];
 
-// Filter: Search
-var searchInput = document.querySelector('.main-nav-search__input');
-//var magnifierButton = document.querySelector('.fa-search:before');
-var searchFilter = searchInput.value.toUpperCase();
-// searchInput.addEventListener('keypress', function (e) {
-//     var key = e.which || e.keyCode;
-//     if (key === 13) {
-//         positiveSearch();
-//     }
-// });
-// magnifierButton.addEventListener('click', function() {positiveSearch()});
+// Loading Books on Page
+var libraryArea = document.querySelector('.main-content');
+var aBookArea = document.querySelector('.book-view');
 
-var foundByTitle = books.forEach(function (element) {
-    element.title.toUpperCase();
-});
-var foundByFirstName = books.forEach(function (element) {
-    element.author.firstName.toUpperCase();
-});
-var foundByLastName = books.forEach(function (element) {
-    element.author.lastName.toUpperCase();
-});
+function loadBooks() {
+    var bookBorder = '';
+    var bookContent = '';
 
-// var found = function () {
-//     if (searchFilter == foundByFirstName || foundByLastName || foundByTitle) {
-//         return true;
-//     } else {
-//         return false;
-//     }
-// };
+    for (var key in books) {
+        bookBorder+= '<div class="book-view">' + bookContent + '</div>';
 
-var positiveSearchTitle = function (searchFilter) {
-    return books.filter(function (el) {
-        return el.title.toUpperCase().indexOf(searchFilter.toUpperCase()) > -1; 
-    });
-};
-var positiveSearchFirstName = function (searchFilter) {
-    return books.filter(function (el) {
-        return el.author.firstName.toUpperCase().indexOf(searchFilter.toUpperCase()) > -1;
-    });
-};
-var positiveSearchLastName = function (searchFilter) {
-    return books.filter(function (el) {
-        return el.author.lastName.toUpperCase().indexOf(searchFilter.toUpperCase()) > -1;
-    });
-};
+        bookContent+= '<div class="book-view__cover">' + books[key].image_url + '</div>';
+        bookContent+= '<div class="book-view__title">' + books[key].title + '</div>';
+        bookContent+= '<div class="book-view__author">' + books[key].author + '</div>';
+        bookContent+= '<div class="book-view__rating">' + books[key].rating + '</div>';
 
-function arrayUnique(array) {
-    var a = array.concat();
-    for (var i = 0; i < a.length; ++i) {
-        for (var j = i + 1; j < a.length; ++j) {
-            if (a[i] === a[j])
-                a.splice(j--, 1);
-        }
     }
-
-    return a;
+    libraryArea.innerHTML(bookBorder);
+    aBookArea.innerHTML(bookContent);
 }
-
-var positiveSearchName = arrayUnique(positiveSearchFirstName.concat(positiveSearchLastName));
-var positiveSearch = arrayUnique(positiveSearchTitle.concat(positiveSearchName));
-
-var positiveSearch = Array.from(new Set([...positiveSearchFirstName, ...positiveSearchLastName, ...positiveSearchTitle]));
-
-var positiveSearch = function (searchFilter) {
-    return books.filter(function () {
-        if (searchFilter == foundByFirstName || foundByLastName || foundByTitle)
-            return true;
-    });
-};
-
-// Filter: Buttons
-//// get Buttons
-var allBooksButton = document.querySelector('#allBooksButton');
-var recentButton = document.querySelector('#recentButton');
-var mostPopularButton = document.querySelector('#mostPopularButton');
-var freeBooksButton = document.querySelector('#freeBooksButton');
-
-//// add Events
